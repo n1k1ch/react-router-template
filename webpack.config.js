1850ce4c9007f274f589106f1865b3bf29135c7a
Nikita Cherevkov <ncherevkov@csod.com>
2016-03-26 00:02:40 +0200

    initial commit (4 parts)

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: './src',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 5555
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    }
+};
